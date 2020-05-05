05c62664050803aac94d589bf86c09a6e0cfa6fb
Drew-Smith-TX <buddyjasmith@gmail.com>
2020-05-05 10:27:50 -0500

    end of section7

--- DatingApp-Spa/src/app/app.module.ts
+++ DatingApp-Spa/src/app/app.module.ts
@@ -9,14 +9,20 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './Home/Home.component';
 import { RegisterComponent } from './register/register.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 @NgModule({
    declarations: [
       AppComponent,
-      
-     
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserAnimationsModule,
@@ -24,6 +30,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes),
    ],
    providers: [
       AuthService
--- DatingApp-Spa/src/app/nav/nav.component.ts
+++ DatingApp-Spa/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,8 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
   constructor(public auth: AuthService,
-              private alert: AlertifyService) { }
+              private alert: AlertifyService,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -20,6 +22,8 @@ export class NavComponent implements OnInit {
         this.alert.success('Logged in sucessfully');
       }, error => {
         this.alert.error('error: Failed to LOGIN');
+      }, () => {
+        this.router.navigate(['/members']);
       });
   }
   loggedIn(){
@@ -28,5 +32,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alert.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
